6d207d498213db48c3cb389145a4ba99189c6e7a
kuoppa55 <20157329+kuoppa55@users.noreply.github.com>
2025-03-20 11:13:59 -0400

    refactored observation into its own component, added more styling to wind/rain/observations to be consistent with temps

--- client/src/components/Today/Today.tsx
+++ client/src/components/Today/Today.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Today.css'
 import { WeatherToday } from '../../types/types'
 import TodayForecast from '../TodayForecast/TodayForecast'
+import TodayObserved from '../TodayObserved/TodayObserved'
 
 const Today = (props: WeatherToday) => {
     const {timeOfDay, shortCast, 
@@ -18,18 +19,8 @@ const Today = (props: WeatherToday) => {
                 lowTemp={forecastLowTemp.value} highTemp={forecastHighTemp.value} nowTemp={observedTemp}/>
             </div>
             <div className="todayChild" id="todayObservedContainer">
-                <h2 className="latestObservationsLabel">Latest Observation</h2>
-                <h4 className="stationInformation">{stationId} {stationName}</h4>
-                <div className="observationsGrid">
-                    <div className="nowWind">
-                        Wind: {observedWind.windSpeed.toFixed(1)} mph from {observedWind.windDirection}
-                    </div>
-                    {observedHumidity &&  <div className="nowHumidity">Humidity: {observedHumidity.toFixed(1)}%</div>}
-                    {observedGust && <div className="nowGust">Gust: {observedGust.toFixed(1)} mph</div>}
-                    {observedVisbility && <div className="nowVis">Visibility: {observedVisbility} miles</div>}
-                    {observedPressure && <div className="nowPressure">Pressure: {Math.round(observedPressure)} MB</div>}
-                </div>
-                <p className="metarString">{metarString}</p>
+                <TodayObserved wind={observedWind} humidity={observedHumidity} visibility={observedVisbility} pressure={observedPressure}
+                gust={observedGust} stationId={stationId} stationName={stationName} metar={metarString} />
             </div>
         </div>
     )
---
+++ client/src/components/TodayObserved/TodayObserved.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Wind } from '../../types/types'
+import './TodayObserved.css'
+
+interface TodayObservedProps {
+    stationId: string,
+    stationName: string,
+    wind: Wind,
+    humidity: number,
+    gust: number,
+    visibility: number,
+    pressure: number,
+    metar: string,
+}
+const TodayObserved = (props: TodayObservedProps) => {
+    const {stationId, stationName, wind, humidity, gust, visibility, pressure, metar} = props
+    return (
+        <div className="todayObserved">
+             <h2 className="latestObservationsLabel">Latest Observation</h2>
+                <h4 className="stationInformation">{stationId} {stationName}</h4>
+                <div className="observationsGrid">
+                    <div className="nowWind">
+                        Wind: {wind.windSpeed.toFixed(1)} mph from {wind.windDirection}
+                    </div>
+                    {humidity &&  <div className="nowHumidity">Humidity: {humidity.toFixed(1)}%</div>}
+                    {gust && <div className="nowGust">Gust: {gust.toFixed(1)} mph</div>}
+                    {visibility && <div className="nowVis">Visibility: {visibility} miles</div>}
+                    {pressure && <div className="nowPressure">Pressure: {Math.round(pressure)} MB</div>}
+                </div>
+                <p className="metarString">{metar}</p>
+        </div>
+    )
+}
+
+export default TodayObserved
