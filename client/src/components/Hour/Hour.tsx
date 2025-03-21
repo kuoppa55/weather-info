727f1beadd95c2274429d99d6383ca7ae74d7c6f
kuoppa55 <20157329+kuoppa55@users.noreply.github.com>
2025-03-21 10:09:49 -0400

    added better styling to hourly forecasts

--- client/src/components/Hour/Hour.tsx
+++ client/src/components/Hour/Hour.tsx
@@ -1,30 +1,52 @@
 import React from 'react'
+import './Hour.css'
 import { HourlyPeriod } from '../../types/types'
 
 const Hour = (props: HourlyPeriod) => {
     const {shortCast, temperature, precipProb, relativeHumidity, wind, startTime} = props
     return (
         <div className="hour">
-            <div>
+            <div className="hourTime">
                 {startTime}
             </div>
-            <div>
-                {shortCast}
-            </div>
-            <div>
-                {temperature.value} F
-            </div>
-            <div>
-                {precipProb}% chance of rain
-            </div>
-            <div>
-                {relativeHumidity}% humidity
-            </div>
-            <div>
-                Wind blowing {wind.windSpeed} from {wind.windDirection}
-            </div>
-            <div>
-                ============================
+            <div className="hourForecastDetails">
+                <div className="hourForecastItem">
+                    <div className="hourForecastItemValue">
+                        {shortCast}
+                    </div>
+                </div>
+                <div className="hourForecastItem">
+                    <div className="hourForecastItemTitle">
+                        Temp
+                    </div>
+                    <div className="hourForecastItemValue">
+                        {temperature.value}
+                    </div>
+                </div>
+                <div className="hourForecastItem">
+                    <div className="hourForecastItemTitle">
+                        Rain
+                    </div>
+                    <div className="hourForecastItemValue">
+                        {precipProb}%
+                    </div>
+                </div>
+                <div className="hourForecastItem">
+                    <div className="hourForecastItemTitle">
+                        Humidity
+                    </div>
+                    <div className="hourForecastItemValue">
+                        {relativeHumidity}%
+                    </div>
+                </div>
+                <div className="hourForecastItem">
+                    <div className="hourForecastItemTitle">
+                        Wind
+                    </div>
+                    <div className="hourForecastItemValue">
+                        {wind.windSpeed} {wind.windDirection}
+                    </div>
+                </div>
             </div>
         </div>
     )
